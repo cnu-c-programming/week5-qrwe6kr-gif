bb267c323d5ed9367b9c48337acb8af5293b123a
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-04-03 16:47:24 +0900

    Update 4-3.c

---
+++ code/4-3.c
@@ -0,0 +1,19 @@
+/ OBB 코드 고치기
+// index를 5이상을 입력 시 OBB 발생
+// index의 boundary 고려해 3이 입력시 40 출력, 100 입력시 Error! 문자을 출력
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int size = 5;
+    int index;
+    scanf("%d", &index);
+    
+    if (index>=0 && index<=size) {
+        printf("%d", 40);
+    } else {
+        printf("%s", "Error!" );
+    } 
+    
+    return 0;
+}
