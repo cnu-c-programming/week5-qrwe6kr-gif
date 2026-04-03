403af128d5c46ef3d3b2614337f7d5e6819172db
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-04-03 16:07:02 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-
+// 포인터 배열 정렬
+// 문자열 주소 입력된 char* 배열 입력받아 알파벳 순서로 정렬하는 함수 구현
+// 문자열 자체를 옮기는 것X 포인터 배열에 담긴 주소값의 위치만 서로 바꿔서 정
 void sort_string(char **arr, int size) {
     char *temp;
     for (int i=0; i<size-1; i++) {
         for (int j=0; j<size-1-i; j++) {
-            if (int i=0; i<size; i++) {
-                int strcmp(char* arr[i], arr[h])
-                
+            if ((strcmp(arr[j], arr[j+1]) > 0)) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
             
         }
     }
 }
 int main() {
-    char* words[] = {"apple", "ant", "banana", "charry");
+    char* words[] = {"apple", "ant", "banana", "cherry"};
     int size = 4;
     
     sort_string(words, size);
@@ -22,6 +25,6 @@ int main() {
     for (int i=0; i<size; i++) {
         printf("%s\n", words[i]);
     }
-}
     return 0;
 
+}
