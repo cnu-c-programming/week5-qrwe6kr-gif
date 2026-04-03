267f76007e823dcbcb617fd667a8f44549140554
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-04-03 09:54:10 +0900

    Update 2-2.c

---
+++ code/2-2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+void sort_string(char **arr, int size) {
+    char *temp;
+    for (int i=0; i<size-1; i++) {
+        for (int j=0; j<size-1-i; j++) {
+            if (int i=0; i<size; i++) {
+                int strcmp(char* arr[i], arr[h])
+                
+            }
+            
+        }
+    }
+}
+int main() {
+    char* words[] = {"apple", "ant", "banana", "charry");
+    int size = 4;
+    
+    sort_string(words, size);
+    
+    for (int i=0; i<size; i++) {
+        printf("%s\n", words[i]);
+    }
+}
+    return 0;
+
